dddcf18f7372ebdecedb45a7f438f9d7f01879d2
tonitdiep <adiep123@gmail.com>
2021-04-13 17:16:59 -0400

    serializer debugging index and show actions

--- tarot-backend/app/controllers/suits_controller.rb
+++ tarot-backend/app/controllers/suits_controller.rb
@@ -2,19 +2,21 @@ class SuitsController < ApplicationController
 
     def index 
         suits = Suit.all
-        render json: SuitSerializer.new(suits, {include: [:cards]})
-        # render json: suits.to_json(include: {cards: {only:[:created_at]}})
+        # render json: SuitSerializer.new(suits, {include: [:cards]})
+
+        render json: SuitSerializer.new(suits)
+  
         # render json: suits
 
-        # render json: suits.to_json(include: {cards: {only: [:name, :meaning_upright, :meaning_reverse]}}, only: [:name, :id])
+        # render json: suits.to_json(include: {cards: {only: [:name, :id, :meaning_upright, :meaning_reverse]}}, only: [:name, :id])
         
-        # render json: suits.to_json(include: {cards: {only:[:created_at]}})
+       
     end
 
     def show
         suit = Suit.find_by(params[:id])
-        # render json: suit
-        render json: suit.to_json(include: {cards: {only: [:name, :meaning_upright, :meaning_reverse]}}, only: [:name, :id])
+        render json: suit
+        # render json: suit.to_json(include: {cards: {only: [:name, :meaning_upright, :meaning_reverse]}}, only: [:name, :id])
     end
     
    private 
